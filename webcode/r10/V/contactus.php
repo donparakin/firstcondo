cc5b7826f0df2beb3993b37ec0a1f8f9f24a96c9
Don Parakin <don@weaddvalue.ca>
2020-12-03 10:35:55 -0500

    Changed email from web form to Google Forms

--- webcode/r10/V/contactus.php
+++ webcode/r10/V/contactus.php
@@ -38,62 +38,11 @@ class TheView extends V_Layouts_LeftNav1 {
 			<td>(905) 761-9504</td>
 		</tr>
 		<tr>
-			<td class="c1">Email:</td>
-			<td>To reduce spam, our email id is no longer displayed here. Use this instead:
-				<form id="email" class="email" method="post" action="/Email" onsubmit="return checkStuff();">
-					<p><label for="to">To:</label></p>
-					<input id="to" name="to" type="text" value="First Condo Group: More Info" />
-					<p><label for="fromName">Your name:</label></p>
-					<input id="fromName" name="fromName" type="text" />
-					<p><label for="fromPhone">Your phone: (optional)</label></p>
-					<input id="fromPhone" name="fromPhone" type="text" />
-					<p><label for="fromEmail">Your email id: (optional)</label></p>
-					<input id="fromEmail" name="fromEmail" type="text" />
-					<p><label for="subject">Subject:</label></p>
-					<input id="subject" name="subject" type="text" value="More Info" />
-					<p><label for="message">Message:</label></p>
-					<textarea id="message" name="message" wrap="virtual" rows="8"></textarea>
-					<br><br>
-					<input id="sendEmail" name="sendEmail" type="submit" value="  Send Email To First Condo Group  " />
-				</form>
+			<td colspan="2">
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfx6LbtIBIPoqWW0__nii33wwqK88SWijTOJn9bHt25OxOpbg/viewform?embedded=true" width="640" height="805" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 			</td>
 		</tr>
 	</table>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script type="text/javascript">
-		function checkStuff() {
-			isEmpty = /^\s*$/ ;
-			if ( isEmpty.test($("#fromName").val()) ) {
-				alert("Your Name: is blank.\n\nEnter your name (example: John Smith).");
-				$("#fromName").focus();
-				return false;
-			}
-			if ( isEmpty.test($("#fromPhone").val()) && isEmpty.test($("#fromEmail").val()) ) {
-				alert("We cannot reply to you if you do not provide a phone or email id.\n\nEnter your phone or your email id (or both).");
-				$("#fromPhone").focus();
-				return false;
-			}
-			if ( !isEmpty.test($("#fromEmail").val()) ) {
-				emailExp = /^\s*[\w\-\.]+\@[\w\-]+\.[\w\-\.]+\s*$/ ;
-				if ( !emailExp.test($("#fromEmail").val()) ) {
-					alert("Your Email: is invalid.\n\nEnter an valid email id (example: myUserid@myHost.com).");
-					$("#fromEmail").focus();
-					return false;
-				}
-			}
-			if ( isEmpty.test($("#subject").val()) ) {
-				alert("Subject: is blank.\n\nEnter a subject or title for your message.");
-				$("#subject").focus();
-				return false;
-			}
-			if ( isEmpty.test($("#message").val()) ) {
-				alert("Message: is blank.\n\nEnter your email message.");
-				$("#message").focus();
-				return false;
-			}
-			return true;
-		}
-	</script>
 <?php
 	}
 }
